c1fef9ac7ff69035a8334f28d3b235e338b41aa9
NicolasFerro23 <nicolas.ferro@sptech.school>
2026-05-20 13:35:45 -0300

    scriptTurfe.js

---
+++ scriptTurfe.js
@@ -0,0 +1,13 @@
+function apostar() {
+        let valorapostado = Number(ipt_valor.value);
+
+        let rodadas = 0;
+        
+        if (ipt_valor.value = '') {
+            div_msg.innerHTML = ''
+            
+        } else {
+            
+        }
+
+    }
