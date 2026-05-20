f558b09ce7fe960fd9b43af66236af4f408a954c
NicolasFerro23 <nicolas.ferro@sptech.school>
2026-05-20 17:47:35 -0300

    jogo finalizado com apostas

--- scriptTurfe.js
+++ scriptTurfe.js
@@ -7,15 +7,29 @@ let vt_cavalo5 = [];
 
 let vt_acm = [];
 
+let qtd_fichasatual = 100;
+
+
 
 
 function apostar() {
-    
+    let valor = ipt_valor.value;
+    // PRECISA MOSTRAR A QTD DE FICHAS QTD_FICHASATUAL NA DIV DE APOSTA
+
+    if ( valor <= 0 || valor > qtd_fichasatual) {
+        alert('Quantidade de fichas indisponível')
+    }
+    else {
+
+    }
+
 }
 
-
-
 function jogar() {
+
+    let cavalo = ipt_cavalo.value;
+    let valor = ipt_valor.value;
+
     let cavalo1 = Number(0);
     let cavalo2 = Number(0);
     let cavalo3 = Number(0);
@@ -36,23 +50,23 @@ function jogar() {
     let cavaloSegundo = 0;
     let cavaloTerceiro = 0;
 
-    let i = 0;
+    let i = 0; 
 
-    let intervalo = setInterval(() => {
-
-
-        cavalo1 = Number((Math.random() * 2 + 7).toFixed(1));
+        let intervalo = setInterval(() => {
+            
+            
+            cavalo1 = Number((Math.random() * 2 + 7).toFixed(1));
         cavalo2 = Number((Math.random() * 2 + 7).toFixed(1));
         cavalo3 = Number((Math.random() * 2 + 7).toFixed(1));
         cavalo4 = Number((Math.random() * 2 + 7).toFixed(1));
         cavalo5 = Number((Math.random() * 2 + 7).toFixed(1));
-
+        
         vt_cavalo1.push(cavalo1);
         vt_cavalo2.push(cavalo2);
         vt_cavalo3.push(cavalo3);
         vt_cavalo4.push(cavalo4);
         vt_cavalo5.push(cavalo5);
-
+        
         console.log(`Cavalo 1: ${cavalo1} Cavalo 2: ${cavalo2} Cavalo 3: ${cavalo3} Cavalo 4: ${cavalo4} Cavalo 4: ${cavalo4} Cavalo 5: ${cavalo5}`)
 
         acm1 += cavalo1;
@@ -75,7 +89,7 @@ function jogar() {
             clearInterval(intervalo)
         }
 
-
+        
     }, 1700);
     setTimeout(() => {
         vt_acm.push(acm1);
@@ -91,16 +105,16 @@ function jogar() {
             vt_cavalo4[6],
             vt_cavalo5[6]
         ];
-
+        
         let ultimaVoltaPrimeiro = 9999;
         let ultimaVoltaSegundo = 9999;
         let ultimaVoltaTerceiro = 9999;
-
+        
         for (let i = 0; i < vt_acm.length; i++) {
 
             let tempoAtual = vt_acm[i];
             let ultimaAtual = ultimasVoltas[i];
-
+            
             if (
                 tempoAtual < primeiro ||
                 (
@@ -112,7 +126,7 @@ function jogar() {
                 terceiro = segundo;
                 cavaloTerceiro = cavaloSegundo;
                 ultimaVoltaTerceiro = ultimaVoltaSegundo;
-
+                
                 segundo = primeiro;
                 cavaloSegundo = cavaloPrimeiro;
                 ultimaVoltaSegundo = ultimaVoltaPrimeiro;
@@ -121,7 +135,7 @@ function jogar() {
                 cavaloPrimeiro = i + 1;
                 ultimaVoltaPrimeiro = ultimaAtual;
             }
-
+            
             else if (
                 tempoAtual < segundo ||
                 (
@@ -133,12 +147,12 @@ function jogar() {
                 terceiro = segundo;
                 cavaloTerceiro = cavaloSegundo;
                 ultimaVoltaTerceiro = ultimaVoltaSegundo;
-
+                
                 segundo = tempoAtual;
                 cavaloSegundo = i + 1;
                 ultimaVoltaSegundo = ultimaAtual;
             }
-
+            
             else if (
                 tempoAtual < terceiro ||
                 (
@@ -152,17 +166,41 @@ function jogar() {
                 ultimaVoltaTerceiro = ultimaAtual;
             }
         }
+        
         div_msg.innerHTML += `1º Lugar - Cavalo ${cavaloPrimeiro} com ${primeiro.toFixed(1)}s <br>
         2º Lugar - Cavalo ${cavaloSegundo} com ${segundo.toFixed(1)}s <br> 
-        3º Lugar - Cavalo ${cavaloTerceiro} com ${terceiro.toFixed(1)}s`
+        3º Lugar - Cavalo ${cavaloTerceiro} com ${terceiro.toFixed(1)}s <br>`
+        if ( cavalo == cavaloPrimeiro) {
+            if ( qtd_fichasatual == 0) {
+                qtd_fichasatual += 5
+            }
+            qtd_fichasatual += valor * 150/100
+            console.log(qtd_fichasatual)
+            div_msg.innerHTML+=`<br> O Cavalo ${cavalo} foi campeão, você venceu sua aposta! Receba ${ valor * 50/100} fichas`
+        }
+        else {
+            qtd_fichasatual -= valor 
+            console.log(qtd_fichasatual)
+            div_msg.innerHTML+=`O Cavalo ${cavalo} não foi o melhor dessa vez, você perdeu sua aposta, tente novamente...` 
+        }
     }, 12000);
+    ipt_cavalo.value=0;
+    ipt_valor.value=''
 }
+    
 
 
 
 
 function selecionar() {
+    let cavalo = ipt_cavalo.value;
 
+    if ( cavalo == 0 ) {
+        alert('Selecione um Cavalo')
+    }
+    else {
+
+    }
 
 
 }
